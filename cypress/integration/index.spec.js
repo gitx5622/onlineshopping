3c05c6c9db4b15c00bbed0832557d0b3fda6ebd6
gitx5622 <ggitau5622@gmail.com>
2022-02-18 01:31:46 +0300

    change of test file

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,10 +1,8 @@
 /// <reference types="cypress"/>
 
 describe('it should visits main page', () => {
-    before(() => {
-        cy.visit("http://localhost:3000")
-    })
     it('The react application correctly loads', () => {
+        cy.visit("http://localhost:3000")
         cy.contains("ONLINE SHOPPING KENYA").should("exist")
         cy.contains("fakeStore API").should("exist")
         cy.get("[data-test=okay-button]").click()
