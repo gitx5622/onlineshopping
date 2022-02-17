adf0ce2fd53f9f550bdc63cc3658a0e2cbcb8ef0
gitx5622 <ggitau5622@gmail.com>
2022-02-17 22:26:52 +0300

    changed next js config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfiguration = {
-  trailingSlash: true,
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['fakestoreapi.com'],
-    disableStaticImages: true,
-  },
+    trailingSlash: true,
 };
+
+
 module.exports = withPlugins([optimizedImages, nextConfiguration]);
