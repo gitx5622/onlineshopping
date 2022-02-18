36ec37ca2b62f1b2376a51f1da8f5237fcf44cdf
gitx5622 <ggitau5622@gmail.com>
2022-02-18 15:46:26 +0300

    edited test file spec

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -7,7 +7,5 @@ describe('it should visits main page', () => {
     it('The react application correctly loads', () => {
         cy.contains("ONLINE SHOPPING KENYA").should("exist")
         cy.contains("fakeStore API").should("exist")
-        cy.get("[data-test=okay-button]").click()
-        cy.contains("fakeStore API", {timeout: 7000}).should("not.exist")
     })
 })
