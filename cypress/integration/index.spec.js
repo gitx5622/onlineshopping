dd921c43ff33f09ad395171fae1e5079ee2191bd
gitx5622 <ggitau5622@gmail.com>
2022-02-18 17:45:50 +0300

    git merge

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('it should visits main page', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+    it('The react application correctly loads', () => {
+        cy.contains("ONLINE SHOPPING KENYA").should("exist")
+        cy.contains("All Products").should("exist")
+        cy.contains("Ascending").should("exist")
+    })
+    it('onclick login it should navigste to login url', () => {
+        cy.get('[data-testid="login"]').contains('Login').should("exist")
+        cy.get('[data-testid="login"]').click()
+        cy.url().should('include', '/user/login')
+    })
+})
