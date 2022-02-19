09bf2c1b0695d23572b41eba70c4347767b89f19
gitx5622 <ggitau5622@gmail.com>
2022-02-19 03:09:28 +0300

    changed somes tests

--- cypress/integration/integration-tests/index.spec.js
+++ cypress/integration/integration-tests/index.spec.js
@@ -4,8 +4,8 @@ describe('it should visits main page', () => {
     it('should tests components for navbar, productList and product details', () => {
         cy.visit('/')
         cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).should('exist')
-        cy.get('[data-test="okay-button"]').click()
-        cy.get('[data-test="okay-button"]').contains('Ok').should('not.exist')
+        cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).click()
+        cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).should('not.exist')
         cy.contains("ONLINE SHOPPING KENYA").should("exist")
         cy.contains("All Products").should("exist")
         cy.contains("Products").should("exist")
--- cypress/integration/unit-tests/index.spec.js
+++ cypress/integration/unit-tests/index.spec.js
@@ -3,12 +3,12 @@
 describe('it should visits main page', () => {
     it('The react application correctly loads', () => {
         cy.visit('/')
+        cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).should('exist')
+        cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).click()
+        cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).should('not.exist')
         cy.contains("ONLINE SHOPPING KENYA").should("exist")
         cy.contains("All Products").should("exist")
         cy.contains("Ascending").should("exist")
-        cy.get('[data-test="okay-button"]').contains('Ok', { timeout: 7000}).should('exist')
-        cy.get('[data-test="okay-button"]').click()
-        cy.get('[data-test="okay-button"]').contains('Ok').should('not.exist')
         cy.get('[data-test="login"]').contains('Login').should("exist")
     })
 })
